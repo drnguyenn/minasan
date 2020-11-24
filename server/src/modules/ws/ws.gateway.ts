0ca3e4663161e576594c2e20b29f7027bfbbf62c
duongle26 <leduong26298@gmail.com>
2020-11-25 03:54:36 +0700

    feat: Add logger and handle websocket events

--- server/src/modules/ws/ws.gateway.ts
+++ server/src/modules/ws/ws.gateway.ts
@@ -1,44 +1,57 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+import { WSEvent } from '../../shared/Enums/ws-event.enum';
 import { MessagesService } from '../message/messages.service';
+import { MessageDto, RoomsDto } from './ws.dto';
 
-// NOTE: just a simple version of ws for chatRoom, lack of errors handler, be careful :D
 @WebSocketGateway()
-export class WsGateway {
+export class WsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   constructor(public messageService: MessagesService) {}
+  private logger = new Logger('WebSocket');
 
-  @WebSocketServer()
-  server: Server;
+  @WebSocketServer() server: Server;
 
-  /**
-   * handle request joinRooms from client
-   * @param client
-   * @param payload: { userInfo, roomIds }
-   */
-  @SubscribeMessage('joinRooms')
-  handleJoinRooms(client: Socket, payload: any) {
-    const { userInfo, roomIds } = payload;
-    roomIds.map((roomId: string) => {
-      client.join(roomId);
-      this.server.to(roomId).emit('clientJoinedRoom', userInfo);
+  afterInit(server: Server) {
+    this.logger.log('WebSocket initialized');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.debug(`${client.id} connected`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.debug(`${client.id} disconnected`);
+  }
+
+  @SubscribeMessage(WSEvent.JOIN_ROOMS)
+  handleJoinRooms(@ConnectedSocket() client: Socket, @MessageBody() { roomIds, userId }: RoomsDto) {
+    if (!roomIds || !userId) throw new WsException('Bad Request');
+
+    roomIds.forEach((roomId: number) => {
+      client.join(roomId.toString());
+      this.logger.debug(`User ${userId} joined room ${roomId}`);
+      client.to(roomId.toString()).broadcast.emit(WSEvent.JOINED_ROOM, `User ${userId} joined room ${roomId}`);
     });
   }
 
-  /**
-   * handle message sent from a client to chatRoom
-   * @param client
-   * @param payload: { roomId, msg, senderId }
-   */
-  @SubscribeMessage('sendMsg')
-  async handleSendMsg(client: Socket, payload: any) {
-    const { roomId, msg, senderId } = payload;
-    if (!roomId || !msg || !senderId) {
-      throw new WsException('bad_request');
-    }
-    // Save message to database
-    await this.messageService.saveMessage(roomId, senderId, msg);
-    // Broadcast to other users
-    this.server.to(roomId).emit('broadcastMsg', { senderId, msg });
+  @SubscribeMessage(WSEvent.SEND_MESSAGE)
+  async handleSendMessage(@ConnectedSocket() client: Socket, @MessageBody() { roomId, message, senderId }: MessageDto) {
+    if (!roomId || !message || !senderId) throw new WsException('Bad Request');
+
+    this.logger.debug(`User ${senderId} sends '${message}' to room ${roomId}`);
+    await this.messageService.saveMessage(roomId, message, senderId);
+    client.to(roomId.toString()).broadcast.emit(WSEvent.BROADCAST_MESSAGE, { roomId, message, senderId });
   }
 }
